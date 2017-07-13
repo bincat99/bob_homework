3e92f6dfeb8efe65112040427e0853b397ce23a9
bincat <bincat99@gmail.com>
2017-07-12 22:57:42 -0700

    add hexdump

--- tmp/pcap_test.c
+++ tmp/pcap_test.c
@@ -1,5 +1,60 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#define DUMP_OFFSET 16
+
+void hexdump (const u_char *packet, bpf_u_int32 len)
+{
+  unsigned i = 0;
+
+  while (i < len)
+  {
+    //const char* dump_format = "%8d: %x%x %x%x %x%x %x%x\t%8s\n"
+    printf("%08x: ", i);
+    
+    unsigned j = 0;
+    while (j < DUMP_OFFSET)
+    {
+      if ((i + j < len))
+        printf ("%02x", 0xff & (char) packet[i+j]);
+      else 
+        printf ("  ");
+
+      if ((i + j + 1< len))
+        printf ("%02x ", 0xff & (char) packet[i+j+1]); 
+      else
+        printf ("   ");
+      j += 2;
+    }
+
+    j = 0;
+    while (j < DUMP_OFFSET)
+    {
+      char c = (char) packet[i+j];
+
+      
+      if (c >= 0x20 && c <0x7f)
+        putchar (c);
+
+      else 
+        putchar ('.');
+
+      j++;
+
+      if (! (i + j < len))
+        break;
+    }
+    puts ("");
+    i += 0x10;
+  }
+}
+
+void parse_packet (const u_char* packet)
+{
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -7,7 +62,7 @@ int main(int argc, char *argv[])
   char *dev;      /* The device to sniff on */
   char errbuf[PCAP_ERRBUF_SIZE];  /* Error string */
   struct bpf_program fp;    /* The compiled filter */
-  char filter_exp[] = "port 80";  /* The filter expression */
+  char filter_exp[] = "port 80 or port 443";  /* The filter expression */
   bpf_u_int32 mask;   /* Our netmask */
   bpf_u_int32 net;    /* Our IP */
   struct pcap_pkthdr *header;  /* The header that pcap gives us */
@@ -46,7 +101,9 @@ int main(int argc, char *argv[])
     int res;
     res = pcap_next_ex(handle, &header, &packet);
     /* Print its length */
+    //printf("res -> %d\n", res);
     printf("Jacked a packet with length of [%x]\n", header->caplen);
+    hexdump (packet, header->caplen); 
     /* And close the session */
   }
   pcap_close(handle);
